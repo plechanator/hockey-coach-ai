2a48845ab47a996963206a647717e2667afc6efe
plechanator <plechanator@gmail.com>
2026-02-22 20:44:28 +0100

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -8,9 +8,13 @@ const __dirname = path.dirname(__filename);
 export function serveStatic(app: express.Express) {
   const distPath = path.resolve(__dirname, "../client/dist");
 
+  console.log("Serving static files from:", distPath);
+
+  // static files
   app.use(express.static(distPath));
 
-  app.get("/*", (_req, res) => {
+  // ✅ Express 5 compatible catch-all
+  app.use((req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
