b09b6fb4301fecde65db1bee32dfa5b2fc71a206
plechanator <plechanator@gmail.com>
2026-02-22 20:35:31 +0100

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -8,11 +8,9 @@ const __dirname = path.dirname(__filename);
 export function serveStatic(app: express.Express) {
   const distPath = path.resolve(__dirname, "../client/dist");
 
-  // statické soubory
   app.use(express.static(distPath));
 
-  // SPA fallback (Express 5 kompatibilní)
-  app.use((req, res) => {
+  app.get("/*", (_req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
